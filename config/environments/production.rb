f1ee53c803b03ebc97ac2e80b740c181ed633f55
kinduff <kinduff@protonmail.com>
2022-12-19 17:29:22 +0100

    Removes blackblaze storage

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -40,7 +40,7 @@ Rails.application.configure do
   # config.action_dispatch.x_sendfile_header = "X-Accel-Redirect" # for NGINX
 
   # Store uploaded files on the local file system (see config/storage.yml for options).
-  config.active_storage.service = :backblaze
+  # config.active_storage.service = :backblaze
 
   # Force all access to the app over SSL, use Strict-Transport-Security, and use secure cookies.
   # config.force_ssl = true
