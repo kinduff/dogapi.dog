0105901c57f9ea835ce591e09c501c1aec9d5c9d
kinduff <kinduff@protonmail.com>
2026-08-11 15:48:03 -0600

    test: flesh out breed and group model specs

--- spec/models/breed_spec.rb
+++ spec/models/breed_spec.rb
@@ -4,4 +4,32 @@ require "rails_helper"
 
 RSpec.describe Breed do
   it { is_expected.to belong_to(:group) }
+  it { is_expected.to have_many_attached(:images) }
+
+  describe "store accessors" do
+    let(:breed) { build(:breed, life: {"min" => 10, "max" => 14}) }
+
+    it "reads the prefixed life range" do
+      expect([breed.life_min, breed.life_max]).to eq([10, 14])
+    end
+
+    it "writes back into the jsonb column" do
+      breed.male_weight_min = 30
+      breed.male_weight_max = 40
+
+      expect(breed.male_weight).to eq("min" => 30, "max" => 40)
+    end
+
+    it "keeps the three ranges separate" do
+      breed.female_weight_min = 25
+
+      expect(breed.male_weight_min).to be_nil
+    end
+  end
+
+  describe "defaults" do
+    it "is not hypoallergenic" do
+      expect(create(:breed).hypoallergenic).to be(false)
+    end
+  end
 end
--- spec/models/group_spec.rb
+++ spec/models/group_spec.rb
@@ -4,4 +4,16 @@ require "rails_helper"
 
 RSpec.describe Group do
   it { is_expected.to have_many(:breeds) }
+
+  it "collects the breeds that belong to it" do
+    group = create(:group)
+    breed = create(:breed, group: group)
+    create(:breed, group: create(:group))
+
+    expect(group.breeds).to eq([breed])
+  end
+
+  it "gets a uuid primary key" do
+    expect(create(:group).id).to match(/\A\h{8}-\h{4}-\h{4}-\h{4}-\h{12}\z/)
+  end
 end
