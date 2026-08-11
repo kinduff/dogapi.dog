866c41b3ce27ac82f188224e741ae11a5eeb5ac2
kinduff <kinduff@protonmail.com>
2026-08-11 13:57:27 -0600

    fix: return 404 for raw fact when none exist

--- app/controllers/api/v1/facts_controller.rb
+++ app/controllers/api/v1/facts_controller.rb
@@ -6,7 +6,8 @@ module Api
       def index
         @facts = Fact.order("RANDOM()").limit(get_limit)
         if params[:raw] == "true"
-          render plain: @facts.first.body
+          fact = @facts.first
+          fact.nil? ? head(:not_found) : render(plain: fact.body)
         else
           render json: {facts: @facts.map(&:body), success: true}
         end
